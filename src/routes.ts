7f28bf4c7e41fd0eb9ce18de597f484c1696237b
Nathaniel Tampus <nate@mugnavo.com>
2023-11-26 15:14:36 +0800

    disabled empty routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,6 @@ const router = Router();
 router.get("/", (_, res) => res.send("Hello World!"));
 
 // All the routes linked to their controllers
-router.use("/users"); // TODO
+//router.use("/users"); // TODO
 
 export default router;
