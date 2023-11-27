91da176c36634c541f75c3b3e039ca234b79284c
Nathaniel Tampus <nate@mugnavo.com>
2023-11-27 09:17:42 +0800

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,16 @@
+import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
     out: "./migrations",
     schema: "./src/db/schema.ts",
     breakpoints: true,
+    driver: "mysql2",
+    dbCredentials: {
+        host: process.env.MYSQL_HOST as string,
+        port: Number(process.env.MYSQL_PORT),
+        database: process.env.MYSQL_DATABASE as string,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+    },
 } satisfies Config;
