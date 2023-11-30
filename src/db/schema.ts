1c893118ba1673c3165fad27b2882420009f75e3
Nathaniel Tampus <nate@mugnavo.com>
2023-11-30 21:05:49 +0800

    fix table names for tradeinventory

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -89,10 +89,10 @@ export const tradeInventory = mysqlTable("trade_inventory", {
     tradeGroupId: int("tradegroup_id")
         .notNull()
         .references(() => tradeGroup.id),
-    senderId: int("user_id")
+    senderId: int("sender_id")
         .notNull()
         .references(() => user.id),
-    receiverId: int("user_id")
+    receiverId: int("receiver_id")
         .notNull()
         .references(() => user.id),
     inventoryId: int("inventory_id")
