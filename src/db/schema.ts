bb6bc94000d259dd9d6b00181da2aeb6115399cb
Nathaniel Tampus <nate@mugnavo.com>
2023-11-30 21:46:23 +0800

    fix rating schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -156,10 +156,10 @@ export const notification = mysqlTable("notification", {
 
 export const rating = mysqlTable("rating", {
     id: int("id").primaryKey().autoincrement(),
-    fromUserId: int("user_id")
+    fromUserId: int("fromuser_id")
         .notNull()
         .references(() => user.id),
-    toUserId: int("user_id")
+    toUserId: int("touser_id")
         .notNull()
         .references(() => user.id),
     amount: int("rating").notNull(),
