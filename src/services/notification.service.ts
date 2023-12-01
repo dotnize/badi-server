8ffa875163c4796a3973d4113dac4bd919693b32
Dead <harenzp14@gmail.com>
2023-12-02 05:34:38 +0800

    GET /notification/user/:id   - req.params.id

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,9 +1,27 @@
+import { eq } from "drizzle-orm";
 import type { Request, Response } from "express";
+import { db } from "~/db/drizzle";
+import { notification } from "~/db/schema";
 
 // GET /notification/user/:id   - req.params.id
 export async function getNotificationByUserId(req: Request, res: Response) {
     try {
         // TODO input validation from "req" object, business logic, then respond using "res" object
+
+        const userId = parseInt(req.params.id, 10); // Convert to number
+
+        // Example: Check if userId is provided and is a valid number
+        if (isNaN(userId)) {
+            res.status(400).json({ error: true, message: "Invalid User ID." });
+            return;
+        }
+
+        // Business logic: Retrieve notifications for the specified user
+        const userNotifications = await db.query.notification.findMany({
+            where: eq(notification.userId, userId),
+        });
+
+        res.status(200).json(userNotifications);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: true, message: "Internal server error." });
