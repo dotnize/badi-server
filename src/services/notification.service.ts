ba13b7044aac55f691632c1380850a1a99f0645f
Dead <harenzp14@gmail.com>
2023-12-02 05:51:24 +0800

    // PUT /notification/:id   - req.params.id and req.body

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -2,6 +2,7 @@ import { eq } from "drizzle-orm";
 import type { Request, Response } from "express";
 import { db } from "~/db/drizzle";
 import { notification } from "~/db/schema";
+import type { Notification } from "~/lib/types";
 
 // GET /notification/user/:id   - req.params.id
 export async function getNotificationByUserId(req: Request, res: Response) {
@@ -65,6 +66,22 @@ export async function updateNotification(req: Request, res: Response) {
         // - check if notification exists using id
         // - check if current session user owns the notification (compare req.session.userId and notification.userId)
         // then req.body should contain the new values for the update query
+        const notificationId = parseInt(req.params.id, 10); // Convert to number
+        const updatedNotificationData: Partial<Notification> = req.body;
+
+        // Example: Check if notificationId and updated data are provided
+        if (isNaN(notificationId) || Object.keys(updatedNotificationData).length === 0) {
+            res.status(400).json({ error: true, message: "Invalid request." });
+            return;
+        }
+
+        // Business logic: Update the notification with the new data
+        await db
+            .update(notification)
+            .set(updatedNotificationData)
+            .where(eq(notification.id, notificationId));
+
+        res.status(200).json({ success: true, message: "Notification updated successfully." });
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: true, message: "Internal server error." });
