5c30904255e74cb5bd4b889cc9eefed327b5d744
Nathaniel Tampus <nate@mugnavo.com>
2023-12-11 19:38:46 +0800

    use isDeleted for DELETE notifs

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -69,19 +69,10 @@ export async function updateNotification(req: Request, res: Response) {
             return;
         }
 
-        // If is_deleted is included in the update, set it accordingly
-        if ("is_deleted" in updatedNotificationData && updatedNotificationData["is_deleted"]) {
-            await db
-                .update(notification)
-                .set({ isDeleted: true }) // Change to isDeleted
-                .where(eq(notification.id, notificationId));
-        } else {
-            // Otherwise, update other fields
-            await db
-                .update(notification)
-                .set(updatedNotificationData)
-                .where(eq(notification.id, notificationId));
-        }
+        await db
+            .update(notification)
+            .set(updatedNotificationData)
+            .where(eq(notification.id, notificationId));
 
         res.status(200).json({ success: true, message: "Notification updated successfully." });
     } catch (err) {
@@ -97,18 +88,36 @@ export async function deleteNotification(req: Request, res: Response) {
         // validation examples:
         // - check if notification exists using id
         // - check if current session user owns the notification (compare req.session.userId and notification.userId)
-        const notificationId = parseInt(req.params.id, 10); // Convert to number
+        const id = parseInt(req.params.id);
 
         // Example: Check if notificationId is provided and is a valid number
-        if (isNaN(notificationId)) {
+        if (isNaN(id)) {
             res.status(400).json({ error: true, message: "Invalid Notification ID." });
             return;
         }
 
-        // Business logic: Delete the notification with the specified ID
-        await db.delete(notification).where(eq(notification.id, notificationId));
+        const userId = req.session.userId;
 
-        res.status(204).end();
+        // find the notif to delete
+        const currentNotif: Notification | undefined = await db.query.notification.findFirst({
+            where: eq(notification.id, id),
+        });
+
+        // if undefined/nonexistent
+        if (!currentNotif) {
+            res.status(404).json({ error: true, message: "Notification does not exist." });
+            return;
+        }
+
+        // check if current session user owns the notif
+        if (currentNotif.userId !== userId) {
+            res.status(403).json({ error: true, message: "You do not own this notification." });
+            return;
+        }
+
+        await db.update(notification).set({ isDeleted: true }).where(eq(notification.id, id));
+
+        res.status(200).json(currentNotif);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: true, message: "Internal server error." });
