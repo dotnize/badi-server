84f75f2e209a9430c3cd2a58d980b9155a3c20ac
Nathaniel Tampus <nate@mugnavo.com>
2023-12-16 20:50:47 +0800

    don't search keywords when finding match for now

--- src/lib/matchFind.ts
+++ src/lib/matchFind.ts
@@ -22,11 +22,11 @@ export async function findMatch(
         // TODO: proper keyword matching, for now only first keyword is searched
         const match: Inventory | undefined = await db.query.inventory.findFirst({
             where: or(
-                ilike(inventory.keywords, `%${keywords[0]}%`),
+                //ilike(inventory.keywords, `%${keywords[0]}%`),
                 ilike(inventory.name, `%${keywords[0]}%`),
                 ilike(inventory.description, `%${keywords[0]}%`),
                 ilike(inventory.preferredOffer, `%${keywords[0]}%`),
-                preferredOffer ? ilike(inventory.keywords, `%${preferredOffer}%`) : undefined,
+                //preferredOffer ? ilike(inventory.keywords, `%${preferredOffer}%`) : undefined,
                 preferredOffer ? ilike(inventory.name, `%${preferredOffer}%`) : undefined,
                 preferredOffer ? ilike(inventory.description, `%${preferredOffer}%`) : undefined,
                 preferredOffer ? ilike(inventory.preferredOffer, `%${preferredOffer}%`) : undefined
@@ -41,7 +41,7 @@ export async function findMatch(
 
         // debugging
         console.log(`found match ${match.id} for ${id}`);
-        console.log(`match keywords: ${match.keywords}`);
+        //console.log(`match keywords: ${match.keywords}`);
         console.log(`match preferredOffer: ${match.preferredOffer}`);
         console.log(`match name: ${match.name}`);
         console.log(`match description: ${match.description}`);
